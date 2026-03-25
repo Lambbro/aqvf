3e3bdbd50aac085ca6e954abe5010f1df1abeb64
Nguyễn Quang Ánh <100024864+Lambbro@users.noreply.github.com>
2026-03-25 19:20:16 +0700

    Delete AQFV directory

--- AQFV/Modules/aqvf/modules/pedagogical_layer.py
+++
@@ -1,9 +0,0 @@
-class PedagogicalLayer:
-
-    def map_bloom_consistency(self, predicted_bloom, labeled_bloom):
-        return predicted_bloom == labeled_bloom
-
-    def map_to_clo(self, question, clo_text):
-        # đơn giản: keyword match
-        score = sum([1 for w in clo_text.split() if w.lower() in question.lower()])
-        return score
--- AQFV/Modules/clo_extraction/extractor.py
+++
@@ -1,8 +0,0 @@
-from clo_parser import extract_clean_clos
-
-clo_list = extract_clean_clos(r"G:\My Drive\Hoạt động nghiên cứu\Đề tài\15032026_AI nâng cao hiệu quả giáo dục\AI bloom taxonomy\Modules\aqvf\data\OOP.pdf")
-
-for clo in clo_list:
-    print(clo["clo_id"])
-    print(clo["description"][:200])
-    print("-" * 50)
